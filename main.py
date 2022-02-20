80ebf0166ed89ac664fee03522a57bfc5bf8c5c0
Nowmaan <85443208+Nowmaan@users.noreply.github.com>
2022-02-21 00:25:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+# 1 First we write the simple eg of Division You will get the output as 4.0.
+
+# def div(num1,num2):
+#     print(num1/num2)
+#
+# div(16,4)
+
+# 2 As we see in 1 it is simple division answer but what if we change the denomenator higher than the numerator we get 0.25 thats where we want to change,
+# we want to give a simple logic to swap the numerator to denomenator , as if we give any sequence of numbers.
+
 # def div(num1,num2):
 #     print(num1/num2)
 #
 # div(4,16)
 
+# 3 We have given a logic in if statement to swap the numbers , but what if we don't want to change that particular function.
 
 # def div(num1,num2):
 #     if num1<num2:
@@ -10,6 +21,9 @@
 #     print(num1/num2)
 #
 # div(4,16)
+
+
+# 4 In that time we use Decorators in the code we use a new function into the wrap-function to add the if statement.
 
 def div(num1,num2):
     print(num1/num2)
